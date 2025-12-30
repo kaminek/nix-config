09497d24a3b697ef5c2356e50fcfe54ab3910ded
Amine Kherbouche <kaminek92@gmail.com>
2025-12-30 11:47:16 +0100

    chore: switch to mise
    Signed-off-by: Amine Kherbouche <kaminek92@gmail.com>

--- modules/apps.nix
+++ modules/apps.nix
@@ -32,7 +32,7 @@
     ruby
     rustc
     cargo
-    uv
+    mise
     docker
     terraform
     tflint
@@ -109,7 +109,7 @@
     brews = [
       "curl" # no not install curl via nixpkgs, it's not working well on macOS!
       "aria2" # download tool
-      "httpie" # http client
+      "xh" # http client (rust-based httpie alternative)
       "tfenv" # Terraform version manager
       "zplug" # Zsh plugin manager
       "mysql-client"
